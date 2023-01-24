4019a09562e0ab1243fd7320cab64d5b4bb54642
siggib@htzn <siggib007@users.noreply.github.com>
2023-01-24 22:17:53 +0000

    DB Create Script

---
+++ CreateDB.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE `phpSecrets` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci */;
+USE `phpSecrets`;
+
+CREATE TABLE `tblSecrets` (
+  `vcGUID` varchar(99) NOT NULL,
+  `vcSecret` varchar(9999) NOT NULL,
+  `dtExpiration` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+GRANT
+    USAGE ON `phpSecrets`.* TO `phpsecret` @`localhost` IDENTIFIED BY PASSWORD 'SetStrongPassword2!!!!'
+GRANT
+  SELECT,
+  INSERT,
+  UPDATE,
+  DELETE,
+  REFERENCES,
+  LOCK TABLES,
+  EXECUTE,
+  SHOW
+  VIEW,
+  TRIGGER
+  ON `phpSecrets`.* TO `phpsecret` @`localhost`
