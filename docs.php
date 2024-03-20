691e2ee6fc95a69a6cd42662c5673ea570488699
Siggi Apacheweb <siggib007@users.noreply.github.com>
2024-03-20 16:28:34 +0000

    documentation

--- docs.php
+++ docs.php
@@ -11,8 +11,19 @@
   require("header.php");
 
   printpg("Documenation","h1");
-  printpg("Working on documenting this great tool.","largebox");
-  printpg("In the meantime reach out to <a href='mailto:$SupportEmail'>$SupportEmail</a> with any questions or concerns.","largebox");
+  printpg("This is a site to facilitate secret sharing in a very secure manner. " .
+  "The data you entered is encrypted using AES-256-CTR encryption, which is one of the strongest encryptions available, " .
+  "using a 35 byte password, then stored in a database. The password is displayed on the screen and not stored anywhere." .
+  "The entry is stored using a GUID as the ID, making it unlikely that the ID will be guessed." .
+  "To retrieve the secret you need to provide the GUID as well as the 35 byte password on the Fetch screen. " .
+  "If the GUID and the password is correct the item will be fetched from the database, decrypted, displayed, then deleted. " .
+  "This means the entry can only be retrieved once before it is lost.","largebox");
+  printpg("You can also specify an expiration date. If the entry isn't retrieved within the specified number of days, " .
+  "the entry is permently deleted","largebox");
+  printpg("<br>The source code for this system is open source and can be retrieved here " .
+  "<a href='https://github.com/siggib007/secret'>https://github.com/siggib007/secret</a>","largebox");
+
+  printpg("<br>Feel free to reach out to <a href='mailto:$SupportEmail'>$SupportEmail</a> with any questions or concerns.","largebox");
 
   require("footer.php");
 ?>
